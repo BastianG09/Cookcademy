e6bb17e332507b7f62c2c9ff8fa69790356327d7
BastianG09 <105691598+BastianG09@users.noreply.github.com>
2022-07-11 15:49:17 -0500

    Add files via upload

---
+++ Cookcademy/Cookcademy/Cookcademy/CookcademyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookcademyApp.swift
+//  Cookcademy
+//
+//  Created by Sebastian Corte on 6/20/22.
+//
+
+import SwiftUI
+
+@main
+struct CookcademyApp: App {
+  var body: some Scene {
+    WindowGroup {
+        MainTabView()
+    }
+  }
+}
